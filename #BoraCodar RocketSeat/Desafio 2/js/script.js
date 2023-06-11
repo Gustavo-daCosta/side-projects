ed71768ce4c9cd519a6c21d1899f2370cace5486
Gustavo-daCosta <80484719+Gustavo-daCosta@users.noreply.github.com>
2023-06-11 13:45:56 -0300

    Desafio 2 - Início do projeto

---
+++ #BoraCodar RocketSeat/Desafio 2/js/script.js
@@ -0,0 +1,4 @@
+function visualizacaoSofa() {
+    document.getElementById("visualization-icon").classList.toggle("icone-fechar");
+    document.getElementById("img-sofa").classList.toggle("gif-sofa");
+}
